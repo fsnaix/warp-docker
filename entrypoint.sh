2c5e2762577bfd75ec6b6a4bf20be47209bfa9a0
fsnaix <truonggiangsn@gmail.com>
2026-06-03 19:09:23 +0700

    Refactor entrypoint.sh for improved clarity and structure

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,15 +6,15 @@ set -e
 # create a tun device if not exist
 # allow passing device to ensure compatibility with Podman
 if [ ! -e /dev/net/tun ]; then
-    sudo mkdir -p /dev/net
-    sudo mknod /dev/net/tun c 10 200
-    sudo chmod 600 /dev/net/tun
+  sudo mkdir -p /dev/net
+  sudo mknod /dev/net/tun c 10 200
+  sudo chmod 600 /dev/net/tun
 fi
 
 # start dbus
 sudo mkdir -p /run/dbus
 if [ -f /run/dbus/pid ]; then
-    sudo rm /run/dbus/pid
+  sudo rm /run/dbus/pid
 fi
 sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf
 
@@ -26,53 +26,56 @@ sleep "$WARP_SLEEP"
 
 # if /var/lib/cloudflare-warp/reg.json not exists, setup new warp client
 if [ ! -f /var/lib/cloudflare-warp/reg.json ]; then
-    # if /var/lib/cloudflare-warp/mdm.xml not exists or REGISTER_WHEN_MDM_EXISTS not empty, register the warp client
-    if [ ! -f /var/lib/cloudflare-warp/mdm.xml ] || [ -n "$REGISTER_WHEN_MDM_EXISTS" ]; then
-        warp-cli registration new && echo "Warp client registered!"
-        # if a license key is provided, register the license
-        if [ -n "$WARP_LICENSE_KEY" ]; then
-            echo "License key found, registering license..."
-            warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
-        fi
+  # if WARP_CONNECTOR_TOKEN is provided, register as WARP Connector
+  if [ -n "$WARP_CONNECTOR_TOKEN" ]; then
+    echo "Connector token found, registering as WARP Connector..."
+    warp-cli connector new "$WARP_CONNECTOR_TOKEN" && echo "WARP Connector registered!"
+  # otherwise use standard registration
+  elif [ ! -f /var/lib/cloudflare-warp/mdm.xml ] || [ -n "$REGISTER_WHEN_MDM_EXISTS" ]; then
+    warp-cli registration new && echo "Warp client registered!"
+    # if a license key is provided, register the license
+    if [ -n "$WARP_LICENSE_KEY" ]; then
+      echo "License key found, registering license..."
+      warp-cli registration license "$WARP_LICENSE_KEY" && echo "Warp license registered!"
     fi
-    # connect to the warp server
-    warp-cli --accept-tos connect
+  fi
+  # connect to the warp server
+  warp-cli connect
 else
-    echo "Warp client already registered, skip registration"
+  echo "Warp client already registered, skip registration"
 fi
 
 # disable qlog if DEBUG_ENABLE_QLOG is empty
 if [ -z "$DEBUG_ENABLE_QLOG" ]; then
-    warp-cli --accept-tos debug qlog disable
+  warp-cli --accept-tos debug qlog disable
 else
-    warp-cli --accept-tos debug qlog enable
+  warp-cli --accept-tos debug qlog enable
 fi
 
 # if WARP_ENABLE_NAT is provided, enable NAT and forwarding
 if [ -n "$WARP_ENABLE_NAT" ]; then
-    # switch to warp mode
-    echo "[NAT] Switching to warp mode..."
-    warp-cli --accept-tos mode warp
-    warp-cli --accept-tos connect
+  # switch to warp mode
+  echo "[NAT] Switching to warp mode..."
+  warp-cli --accept-tos mode warp
+  warp-cli connect
 
-    # wait another seconds for the daemon to reconfigure
-    sleep "$WARP_SLEEP"
+  # wait another seconds for the daemon to reconfigure
+  sleep "$WARP_SLEEP"
 
-    # enable NAT
-    echo "[NAT] Enabling NAT..."
-    sudo nft add table ip nat
-    sudo nft add chain ip nat WARP_NAT { type nat hook postrouting priority 100 \; }
-    sudo nft add rule ip nat WARP_NAT oifname "CloudflareWARP" masquerade
-    sudo nft add table ip mangle
-    sudo nft add chain ip mangle forward { type filter hook forward priority mangle \; }
-    sudo nft add rule ip mangle forward tcp flags syn tcp option maxseg size set rt mtu
-
-    sudo nft add table ip6 nat
-    sudo nft add chain ip6 nat WARP_NAT { type nat hook postrouting priority 100 \; }
-    sudo nft add rule ip6 nat WARP_NAT oifname "CloudflareWARP" masquerade
-    sudo nft add table ip6 mangle
-    sudo nft add chain ip6 mangle forward { type filter hook forward priority mangle \; }
-    sudo nft add rule ip6 mangle forward tcp flags syn tcp option maxseg size set rt mtu
+  # enable NAT
+  echo "[NAT] Enabling NAT..."
+  sudo nft add table ip nat
+  sudo nft add chain ip nat WARP_NAT { type nat hook postrouting priority 100 \; }
+  sudo nft add rule ip nat WARP_NAT oifname "CloudflareWARP" masquerade
+  sudo nft add table ip mangle
+  sudo nft add chain ip mangle forward { type filter hook forward priority mangle \; }
+  sudo nft add rule ip mangle forward tcp flags syn tcp option maxseg size set rt mtu
+  sudo nft add table ip6 nat
+  sudo nft add chain ip6 nat WARP_NAT { type nat hook postrouting priority 100 \; }
+  sudo nft add rule ip6 nat WARP_NAT oifname "CloudflareWARP" masquerade
+  sudo nft add table ip6 mangle
+  sudo nft add chain ip6 mangle forward { type filter hook forward priority mangle \; }
+  sudo nft add rule ip6 mangle forward tcp flags syn tcp option maxseg size set rt mtu
 fi
 
 # start the proxy
